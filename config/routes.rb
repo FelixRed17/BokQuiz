b9def49af069194dbdb3ef274a03e126ca615605
glucode-abdul <abdul01@glucode.com>
2025-09-22 13:58:08 +0200

    Add route for creating games with state retrieval in API v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      
       resources :games, only: [ :create ], param: :code do
         member do
           get  :state
