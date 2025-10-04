30d9f30a55644b3afd6e703793b226f918fbd267
FelixRed17 <felix@glucode.com>
2025-10-05 00:52:48 +0200

    socket fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,6 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
   config.action_cable.url = "wss://excess-seana-felix-glucode-0704cdd3.koyeb.app/cable"
+  config.action_cable.allowed_request_origins = ['http://localhost:5173', /http:\/\/localhost.*/]
+  config.action_cable.disable_request_forgery_protection = true
 end
