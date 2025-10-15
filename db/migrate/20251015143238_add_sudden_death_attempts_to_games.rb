c4763111af1418323be9b533d20924bff7c8a294
FelixRed17 <felix@glucode.com>
2025-10-15 17:02:44 +0200

    deleted rails migrate files causing error

--- db/migrate/20251015143238_add_sudden_death_attempts_to_games.rb
+++
@@ -1,7 +0,0 @@
-class AddSuddenDeathAttemptsToGames < ActiveRecord::Migration[8.0]
-  def change
-    add_column :games, :sudden_death_attempts, :integer, default: 0, null: false
-    add_column :games, :sudden_death_started_at, :datetime
-    
-  end
-end
