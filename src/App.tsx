c39cfd97e50e588f44f4d6961e9e169aced2e915
glucode-abdul <abdul01@glucode.com>
2025-10-29 16:27:59 +0200

    Fixed stuff

--- src/App.tsx
+++ src/App.tsx
@@ -11,39 +11,33 @@ import HostLeaderboardPage from "./Pages/HostLeaderboardPage/HostLeaderboardPage
 import PlayerRoundResultPage from "./Pages/PlayerRoundResultPage/PlayerRoundResultPage";
 import WinnerPage from "./Pages/WinnerPage/WinnerScreen";
 import WaitingSuddenDeathPage from "./Pages/WaitingSuddenDeathPage/WaitingSuddenDeathPage";
-import CountDown from "./Pages/CountDownPage/CountDown";
-
-
 
 
 function App() {
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<WelcomePageWrapper />} />
-    //     <Route path="/admin" element={<SpringbokGameHost />} />
-    //     <Route path="/player" element={<PlayerRegistration />} />
-    //     <Route path="/lobby/:code" element={<LobbyScreen />} />
-    //     <Route path="/player/lobby/:code" element={<PlayerLobbyPage />} />
-    //     <Route path="/game/:code/question" element={<QuizPage />} />
-    //     <Route path="/game/:code/host" element={<HostQuizView />} />
-    //     <Route
-    //       path="/game/:code/leaderboard"
-    //       element={<HostLeaderboardPage />}
-    //     />
-    //     <Route
-    //       path="/game/:code/round-result"
-    //       element={<PlayerRoundResultPage />}
-    //     />
-    //     <Route
-    //       path="/game/:code/sudden-death-wait"
-    //       element={<WaitingSuddenDeathPage />}
-    //     />
-    //     <Route path="/game/:code/winner" element={<WinnerPage />} />
-    //   </Routes>
-    // </BrowserRouter>
     <BrowserRouter>
-    <LobbyScreen/>
+      <Routes>
+        <Route path="/" element={<WelcomePageWrapper />} />
+        <Route path="/admin" element={<SpringbokGameHost />} />
+        <Route path="/player" element={<PlayerRegistration />} />
+        <Route path="/lobby/:code" element={<LobbyScreen />} />
+        <Route path="/player/lobby/:code" element={<PlayerLobbyPage />} />
+        <Route path="/game/:code/question" element={<QuizPage />} />
+        <Route path="/game/:code/host" element={<HostQuizView />} />
+        <Route
+          path="/game/:code/leaderboard"
+          element={<HostLeaderboardPage />}
+        />
+        <Route
+          path="/game/:code/round-result"
+          element={<PlayerRoundResultPage />}
+        />
+        <Route
+          path="/game/:code/sudden-death-wait"
+          element={<WaitingSuddenDeathPage />}
+        />
+        <Route path="/game/:code/winner" element={<WinnerPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
